5104a258295caf5074fbbb7d6f638d89273ff187
Dante Bruni <dande90@hotmail.it>
2019-08-30 12:47:12 +0200

    'hope'

---
+++ src/main/java/org/azienda/Confluent_Controller/App.java
@@ -0,0 +1,13 @@
+package org.azienda.Confluent_Controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
