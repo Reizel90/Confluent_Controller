9245a9ac8ceec3b75a5d835435df654d5faeb55b
Dante Bruni <dande90@hotmail.it>
2019-08-30 15:04:20 +0200

    gitignore update and main procedurized

--- src/main/java/org/azienda/Confluent_Controller/MainClass.java
+++ src/main/java/org/azienda/Confluent_Controller/MainClass.java
@@ -1,4 +1,5 @@
 package org.azienda.Confluent_Controller;
+
 import kafka.IKafkaConstants;
 import kafka.KafkaMain;
 
@@ -18,78 +19,73 @@ import io.debezium.connector.sqlserver.SqlServerConnector;
 
 /**
  * Hello world!
- *
  */
-public class MainClass 
-{
-	public static void main(String[] args) {
-		
-		String connection = "192.168.1.189:9092";
-		
-		Properties props0 = new Properties();
-		props0.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		props0.put(ConsumerConfig.GROUP_ID_CONFIG, "myTestGroup");
-		props0.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
-		props0.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		props0.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 5);
-		props0.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-		props0.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-		
-//		Kafka.runProducer();
-		//RunnableConsumer test_local = new RunnableConsumer(props0, "demo");
-		//test_local.run();
-		
-		Properties props1 = new Properties();
-		props1.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, connection);
-		props1.put(ConsumerConfig.GROUP_ID_CONFIG, "myTestGroup");
-		props1.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
-		props1.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		props1.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 5);
-		props1.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-		props1.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+public class MainClass {
 
-//		Kafka.runProducer();
-		//RunnableConsumer test_string = new RunnableConsumer(props1, "test-string-IVA");
-		//test_string.run();
-		
-		
+    public static final String connection = "192.168.1.189:9092";
+    public static final String group = "myTestGroup";
+
+    public static void main(String[] args) {
+
+        /// TODO create and launch a jdbc-sql source connector for sql server
+        Map<String, String> propsql;
+        propsql.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, connection);
+        propsql.compute(, remappingFunction)
+
+        //SqlServerConnection x = new SqlServerConnection((Configuration) props0);
+        SqlServerConnector p = new SqlServerConnector();
+        p.start(propsql);
+
+
+        try {
+            TimeUnit.MINUTES.sleep(5);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    public void local_consumer() {
+        Properties props0 = new Properties();
+        props0.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props0.put(ConsumerConfig.GROUP_ID_CONFIG, group);
+        props0.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props0.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props0.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 5);
+        props0.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props0.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        RunnableConsumer test_local = new RunnableConsumer(props0, "demo");
+        test_local.run();
+    }
+
+    public void remote_string_consumer() {
+        Properties props1 = new Properties();
+        props1.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, connection);
+        props1.put(ConsumerConfig.GROUP_ID_CONFIG, group);
+        props1.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props1.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props1.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 5);
+        props1.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+        props1.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        RunnableConsumer test_string = new RunnableConsumer(props1, "test-string-IVA");
+        test_string.run();
+    }
+
+    public void remote_consumer(){
 		Properties props2 = new Properties();
 		props2.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, connection);
-		props2.put(ConsumerConfig.GROUP_ID_CONFIG, "myTestGroup");
+		props2.put(ConsumerConfig.GROUP_ID_CONFIG, group);
 		props2.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
 		props2.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		props2.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 5);
 		props2.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 		props2.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
-//		Kafka.runProducer();
-		//RunnableConsumer test_asis = new RunnableConsumer(props1, "test2-IVA");
-		//test_asis.run();
-		
-		
-		
-		/// TODO create and launch a jdbc-sql source connector for sql server
-		Map<String, String> propsql;
-		propsql.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, connection);
-		propsql.compute(, remappingFunction)
-		
-		//SqlServerConnection x = new SqlServerConnection((Configuration) props0);
-				SqlServerConnector p = new SqlServerConnector();
-				p.start(propsql);
-
-		
-		
-		try {
-			TimeUnit.MINUTES.sleep(5);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
-
-
+		RunnableConsumer test_asis = new RunnableConsumer(props1, "test2-IVA");
+		test_asis.run();
 	}
-
 
 }
