b66b23ab972ece0d95f416c2433b4fb3fa6e4c9e
Dante Bruni <dande90@hotmail.it>
2019-09-02 15:49:48 +0200

    better commit for addConnector and it's usage example

--- src/main/java/g/liva/AddConnector.java
+++ src/main/java/g/liva/AddConnector.java
@@ -1,13 +1,8 @@
 package g.liva;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.util.EntityUtils;
-
-import java.io.IOException;
 
 import static org.toilelibre.libe.curl.Curl.curl;
-import static org.toilelibre.libe.curl.Curl.$;
 
 public class AddConnector {
 
@@ -23,32 +18,5 @@ public class AddConnector {
         System.out.println(cmd);
         response = curl(cmd);
         return response;
-    }
-
-    public static void usage() throws IOException {
-        String exampleConfig = "{\"name\": \"jdbc-connector\",\n" +
-                "    \"config\": {\n" +
-                "      \"connector.class\": \"io.confluent.connect.jdbc.JdbcSourceConnector\",\n" +
-                "      \"timestamp.column.name\": \"date\",\n" +
-                "      \"incrementing.column.name\": \"id\",\n" +
-                "      \"connection.user\": \"example\",\n" +
-                "      \"connection.password\": \"example\",\n" +
-                "      \"validate.non.null\": \"true\",\n" +
-                "      \"tasks.max\": \"1\",\n" +
-                "      \"table.whitelist\": \"account\",\n" +
-                "      \"mode\": \"incrementing\",\n" +
-                "      \"topic.prefix\": \"db_connector\",\n" +
-                "      \"poll.interval.ms\": \"1\",\n" +
-                "      \"name\": \"jdbc-connector\",\n" +
-                "      \"connection.url\": \"jdbc:postgresql://143.205.114.69:5432/postgres\"\n" +
-                "    } }";
-        exampleConfig = exampleConfig.replace("\n","");
-        exampleConfig = exampleConfig.replace(" ","");
-        exampleConfig = exampleConfig.replace("\t","");
-        System.out.println(exampleConfig);
-        AddConnector ac = new AddConnector("localhost:8083");
-        HttpResponse r = ac.AddConnector(exampleConfig);
-        HttpEntity e = r.getEntity();
-        System.out.println(EntityUtils.toString(e));
     }
 }
